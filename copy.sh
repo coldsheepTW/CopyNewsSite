3af9fca8625c245bfee4d9a0e6331adbcd9c83a6
Cold Sheep <coldsheep@ailabs.tw>
2017-08-11 14:33:07 +0800

    Auto _site update

--- copy.sh
+++ copy.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 H=`pwd`
 date=`date +%Y-%m-%d`
+yesterday=`date -d "yesterday" +%Y-%m-%d`
 
 for page in $H/_pages/*
 do
     fname=`basename $page`
     cp $page $H/_posts/$date-$fname
 done
+rm -f $H/_posts/$yesterday*
